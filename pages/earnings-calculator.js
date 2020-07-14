57a81fc274d4673555e111332b8abe9a96196330
Daniel James <danielcolinjames@gmail.com>
2020-07-14 10:08:35 -0700

    Add Tailwind and Emotion

--- pages/earnings-calculator.js
+++ pages/earnings-calculator.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 import Head from "next/head";
 
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
 function EarningsCalculator({ data }) {
   const [earningsPerDay, setEarningsPerDay] = useState(7);
   const [numberOfHotspots, setNumberOfHotspots] = useState(1);
@@ -29,35 +32,77 @@ function EarningsCalculator({ data }) {
   const totalEstimateInUsd = totalEstimate * hntUsdExchangeRate;
 
   return (
-    <div className="container">
+    <div
+      className="m-4 flex align-center justify-center"
+      css={css`
+        width: auto;
+      `}
+    >
       <Head>
         <title>Helium Hotspot Earnings Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">Helium Hotspot Earnings Calculator</h1>
+      <main className="prose">
+        <h1
+          css={css`
+            max-width: 400px;
+            font-family: Soleil;
+          `}
+          className="pt-20"
+        >
+          Helium Hotspot Earnings Calculator
+        </h1>
         <input
           type="number"
           id="number-of-hotspots-input"
           placeholder={1}
           onChange={handleChange}
+          className={``}
+          css={css`
+            max-width: 250px;
+            width: auto;
+            border: 1px solid #ccc;
+            height: 45px;
+            padding: 1rem;
+            /* margin: 1rem; */
+            font-family: Sen;
+          `}
         />
         <input
           type="number"
           id="earnings-per-day-input"
           placeholder={7}
           onChange={handleChange}
+          css={css`
+            max-width: 250px;
+            width: auto;
+            border: 1px solid #ccc;
+            height: 45px;
+            padding: 1rem;
+            font-family: Sen;
+            /* margin: 1rem; */
+          `}
         />
 
-        <p>
+        <p
+          css={css`
+            max-width: 400px;
+            font-family: Soleil;
+          `}
+        >
           With {numberOfHotspots} hotspot{numberOfHotspots === 1 ? "" : "s"}
           {numberOfHotspots === 1 ? "" : ", each one"} earning roughly{" "}
           {earningsPerDay}
           {" HNT "}
           per day, you'll make {totalEstimate} HNT per year.{" "}
         </p>
-        <p>
+        <p
+          css={css`
+            max-width: 400px;
+            font-family: Soleil;
+          `}
+        >
           That's {totalEstimateInUsd} USD per year at an exchange rate of{" "}
           {hntUsdExchangeRate} USD per HNT token.
         </p>
