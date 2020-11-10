865fcc9e9b5769b5fda8c6abbe77a9d668a60114
Daniel James <danielcolinjames@gmail.com>
2020-11-09 23:57:01 -0800

    Add functionality to chain vars page

--- pages/chain-vars.js
+++ pages/chain-vars.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import NavBar from "../components/core/NavBar";
 import MetaTags from "../components/core/MetaTags";
 
 const ChainVars = ({ chainVars }) => {
+  const [filterText, setFilterText] = useState("");
+
+  const handleFilterTextChange = (event) => {
+    setFilterText(event.target.value);
+  };
+
   const populateDescription = (key) => {
     // Default description
     let description = "[Default description]";
@@ -66,63 +73,116 @@ const ChainVars = ({ chainVars }) => {
               Chain Variables
             </h2>
             <div className="pb-10">
-              <input
-                className="p-2 w-full lg:w-1/4"
-                placeholder="Filter (not functional yet)"
-              />
+              <div className="lg:w-1/2 w-full relative">
+                <button
+                  className="absolute right-0 my-2 mx-2 py-1 px-1"
+                  onClick={() => {
+                    setFilterText("");
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="h-4 w-4 text-gray-500"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+                <input
+                  className="p-2 w-full"
+                  value={filterText}
+                  onChange={handleFilterTextChange}
+                  placeholder="Filter variables"
+                />
+              </div>
             </div>
             <div className="grid grid-cols-2 gap-10z lg:grid-cols-4 lg:p-1 lg:bg-gray-300 lg:rounded-lg">
               {Object.keys(chainVars.data).map((key, index) => {
-                return (
-                  <>
-                    <div
-                      className={`${
-                        index % 2 === 0 ? "lg:bg-gray-100" : ""
-                      } col-span-4 lg:col-span-2 bg-white border px-4 py-2 text-hpgreen-100 font-display font-md break-normal`}
-                    >
-                      <p>{key}</p>
-
-                      <p
+                if (filterText === "" || key.includes(filterText)) {
+                  return (
+                    <>
+                      <div
                         className={`${
                           index % 2 === 0 ? "lg:bg-gray-100" : ""
-                        } text-gray-500 font-body font-sm pb-12z pr-4 break-normal`}
+                        } col-span-4 lg:col-span-2 bg-white border px-4 py-2 text-hpgreen-100 font-display font-md break-normal`}
                       >
-                        {populateDescription(key)}
-                      </p>
-                    </div>
-                    <div className="mb-10 lg:mb-0 col-span-4 lg:col-span-2 flex flex-row justify-between bg-hpblue-900 border border-gray-900 px-4 py-2">
-                      <p
-                        id={key}
-                        className={`pr-4 pt-2 text-hpblue-100 font-body font-md break-normal w-full`}
-                      >
-                        {Array.isArray(chainVars.data[key])
-                          ? "[Need to find a better way to display arrays]"
-                          : chainVars.data[key]}
-                      </p>
-                      <div className="p-1">
-                        <button
-                          className="px-2 py-0 bg-gray-800 hover:bg-gray-700 h-10 rounded-md font-xs text-gray-200"
-                          onClick={() => copyText(key)}
+                        <p>{key}</p>
+
+                        <p
+                          className={`${
+                            index % 2 === 0 ? "lg:bg-gray-100" : ""
+                          } text-gray-500 font-body font-sm pb-12z pr-4 break-normal`}
                         >
-                          <svg
-                            className="w-5 stroke-text text-gray-500"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth={2}
-                              d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-                            />
-                          </svg>
-                        </button>
+                          {populateDescription(key)}
+                        </p>
                       </div>
-                    </div>
-                  </>
-                );
+                      <div className="mb-10 lg:mb-0 col-span-4 lg:col-span-2 flex flex-row justify-between bg-hpblue-900 border border-gray-900 px-4 py-2">
+                        <p
+                          id={key}
+                          className={`pr-4 pt-2 text-hpblue-100 font-body font-md break-normal w-full`}
+                        >
+                          {Array.isArray(chainVars.data[key]) ? (
+                            <div className="break-all">
+                              {chainVars.data[key].map(
+                                (dataArrayItem, index) => {
+                                  return (
+                                    <div className="py-2 flex flex-row">
+                                      <span className="text-gray-700 pr-2">
+                                        {index}
+                                      </span>
+                                      <p
+                                        className={`pb-3 ${
+                                          index + 1 !==
+                                          chainVars.data[key].length
+                                            ? "border-b border-gray-800"
+                                            : ""
+                                        }`}
+                                      >
+                                        {dataArrayItem}
+                                      </p>
+                                    </div>
+                                  );
+                                }
+                              )}
+                            </div>
+                          ) : (
+                            chainVars.data[key]
+                          )}
+                        </p>
+                        <div className="p-1">
+                          <button
+                            className="px-2 py-0 bg-gray-800 hover:bg-gray-700 h-10 rounded-md font-xs text-gray-200"
+                            onClick={() => copyText(key)}
+                          >
+                            <svg
+                              className="w-5 stroke-text text-gray-500"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </>
+                  );
+                } else {
+                  return <></>;
+                }
               })}
             </div>
           </div>
